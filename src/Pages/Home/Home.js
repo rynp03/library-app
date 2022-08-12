4b780933c5459f1e78eec6ef4cb9826e26306493
Aryan <aryan.ritesh002@gmail.com>
2022-08-12 17:05:33 +0530

    /

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "../../Components/Header/Header";
 import { Outlet } from "react-router-dom";
 
 const Home = () => {
   return (
     <main>
-      <Header />
       <Outlet />
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -7,17 +7,18 @@ import About from "./Pages/About/About";
 import BookList from "./Components/BookList/BookList";
 import BookDetails from "./Components/BookDetails/BookDetails";
 import { AppProvider } from "./context";
+import Header from "./Components/Header/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AppProvider>
     <BrowserRouter>
+    <Header />
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/book" element={<BookList />} />
           <Route path="/book/:id" element={<BookDetails />} />
-        </Route>
       </Routes>
     </BrowserRouter>
   </AppProvider>
