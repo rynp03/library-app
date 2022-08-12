961a331c1f8b8d5265fdbe5bbd13136a56c4036d
Aryan <aryan.ritesh002@gmail.com>
2022-08-12 17:08:48 +0530

    /

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../../Components/Header";
+import Header from "../../Components/Header/Header";
 
 const Home = () => {
   return (
